efa6302d384173d706531bab1ee3d1090dd21f55
Willacy <wannicy@gmail.com>
2025-07-16 21:30:41 -0300

    Update 0.5
    Remoção do botão salvar

--- app/src/main/java/com/example/equacao2grau/MainActivity.java
+++ app/src/main/java/com/example/equacao2grau/MainActivity.java
@@ -111,11 +111,6 @@ public class MainActivity extends AppCompatActivity {
             binding.valorA.requestFocus();
         });
 
-        // botão salvar
-        binding.btnSalvar.setOnClickListener(view -> {
-                    
-                });
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
